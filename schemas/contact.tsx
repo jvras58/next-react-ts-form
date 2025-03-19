f7042063e28bd4b88aa2a04b3caea1ae3ac28a32
jvras58 <jvras@cin.ufpe.br>
2025-03-19 18:03:00 -0300

    add: components shadcnui

--- schemas/contact.tsx
+++ schemas/contact.tsx
@@ -1,8 +1,13 @@
 import { z } from "zod";
 
 const FormSchema = z.object({
-  nome: z.string().min(1, "Nome é obrigatório"),
-  email: z.string().email("Email inválido"),
+  nome: z.string().min(5, "Nome é obrigatório"),
+  email: z.string().min(1, "Email é obrigatório"),
 });
+
+export const FormPlaceholders = {
+  nome: "Digite seu nome completo",
+  email: "Seu endereço de email",
+};
 
 export default FormSchema;
